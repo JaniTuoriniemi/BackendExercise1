beb37496369518b4dc7e1de3c4cbb4af69761ac2
JaniTuoriniemi <janituoriniemi@hotmail.com>
2022-02-07 13:44:27 +0100

    Exercise2

--- BackendExercise/Controllers/People.cs
+++ BackendExercise/Controllers/People.cs
@@ -16,11 +16,11 @@ namespace BackendExercise.Controllers
         public IActionResult People_main(string search, int? id)
         {
             Models.Person person = new Models.Person();
-            
+
             int _id;
             if (search != null)
             {
-               
+
                 List<Models.Person> list = Models.Personlist.Persons;
                 List<Models.Person> a = list.FindAll(x => x.Name.Contains(search));
                 List<Models.Person> b = list.FindAll(x => x.City.Contains(search));
@@ -58,8 +58,8 @@ namespace BackendExercise.Controllers
                 return View(person);
             }
         }
-        
-       [HttpPost]
+
+        [HttpPost]
         public IActionResult People_main(Models.Person person)
         {
             if (ModelState.IsValid)
@@ -74,12 +74,74 @@ namespace BackendExercise.Controllers
 
 
 
-            return View("/Views/People/People_main.cshtml", person);
+            return View( person);
 
+        }
+        [HttpGet]
+        public IActionResult People_WithPartial(string search, int? id)
+        {
+            Models.Person person = new Models.Person();
+
+            int _id;
+            if (search != null)
+            {
+
+                List<Models.Person> list = Models.Personlist.Persons;
+                List<Models.Person> a = list.FindAll(x => x.Name.Contains(search));
+                List<Models.Person> b = list.FindAll(x => x.City.Contains(search));
+                List<Models.Person> c = list.FindAll(x => Convert.ToString(x.Phone).Contains(search));
+
+                List<Models.Person> viewlist = a.Union(b).Union(c).ToList();
+
+                person.OtherPersons = viewlist;
+
+                return View(person);
+            }
+            else if (id != null)
+            {
+                _id = Convert.ToInt32(id);
+                Models.Personlist.RemovePerson(_id);
+
+                if (Models.Personlist.Persons != null)
+                {
+
+                    { person.OtherPersons = Models.Personlist.Persons; }
+                }
+
+
+
+                return View(person);
+            }
+            else
+            {
+                if (Models.Personlist.Persons != null)
+                {
+
+                    { person.OtherPersons = Models.Personlist.Persons; }
+                }
+
+                return View(person);
+            }
+        }
+    
+    [HttpPost]
+    public IActionResult People_WithPartial(Models.Person person)
+    {
+        if (ModelState.IsValid)
+        { Models.Personlist.AddPerson(person); }
+
+
+        if (Models.Personlist.Persons != null)
+        {
+
+            { person.OtherPersons = Models.Personlist.Persons; }
         }
 
 
+
+        return View( person);
+
     }
-   
-    }
+}
+}
 
